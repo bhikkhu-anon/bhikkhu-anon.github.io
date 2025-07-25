49c7cfc6617acd77681a6659598a40d8e74c123f
Bhikkhu Anon <adicco.bhikkhu@proton.me>
2025-07-25 14:04:28 +0200

    update header

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'Dhamma Notes';
+export const SITE_DESCRIPTION = 'Various notes on the Dhamma-centered life.';
